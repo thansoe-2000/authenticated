bb43893983ee0298909b2badda6535b8fe653c29
thansoe-2000 <tt6289216@gmail.com>
2024-09-18 07:42:20 -0700

    login/logout/register

--- authenticated/myapp/urls.py
+++ authenticated/myapp/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.index, name="index_page"),
     path('login/', views.loginPage, name='login_page'),
     path('logout/', views.logoutPage, name='logout_page'),
-    path('register/', views.register, name='register_page'),
+    path('register/', views.registerPage, name='register_page'),
     path('profile/', views.profile, name='user_profile_page'),
 ]
--- authenticated/myapp/views.py
+++ authenticated/myapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from .forms import *
+from .models import *
 # Create your views here.
 
 @login_required(login_url='login_page')
@@ -8,6 +10,8 @@ def index(request):
     return render(request, 'layout/index.html')
 
 def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('index_page')
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -22,8 +26,20 @@ def logoutPage(request):
     logout(request)
     return redirect('login_page')
 
-def register(request):
-    return render(request, 'pages/pages-register.html')
+def registerPage(request):
+    if request.user.is_authenticated:
+        return redirect('index_page')
+    else:
+        form = CreateUserForm()
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('login_page')
+        context = {
+                'form':form
+            }
+        return render(request, 'pages/pages-register.html', context)
 
 def profile(request):
     return render(request, 'pages/users-profile.html')
